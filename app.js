0d52e176d8276be1b435eac4476049ee4a8b657d
unknown <aelise17264@gmail.com>
2021-04-14 15:05:16 -0600

    connected get request with articles in database

--- app.js
+++ app.js
@@ -10,9 +10,25 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true})
 
+const articleSchema = {
+    title: String,
+    content: String
+}
 
+const Article = mongoose.model("Article", articleSchema)
 
+app.get("/articles", function(req, res){
+    Article.find({}, function(err, foundArticles){
+        if(!err){
+        // console.log(foundArticles)
+            res.send( foundArticles)
+        }else{
+            res.send(err)
+        }
+    })
+})
 
 
 app.listen(3000, function(){
